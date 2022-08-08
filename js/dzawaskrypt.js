221e3ada86761920749e49dd2693fe3f08138a4a
CZUBIX <47487634+CZUBIX@users.noreply.github.com>
2022-08-08 20:33:53 +0200

    apdejt

--- js/dzawaskrypt.js
+++ js/dzawaskrypt.js
@@ -1,93 +1,38 @@
 const aboutmeButton = document.getElementById("aboutme_button")
 const linksButton = document.getElementById("links_button")
-const aboutme = document.getElementById("aboutme")
-const links = document.getElementById("links")
+const linkList = document.getElementById("links")
 
-const youtube = document.getElementById("youtube")
-const github = document.getElementById("github")
-const discord = document.getElementById("discord")
-const discordPoligon = document.getElementById("discord_poligon")
-const cenzura = document.getElementById("cenzura")
-const steam = document.getElementById("steam")
-const roblox = document.getElementById("roblox")
-const korrumzthegame = document.getElementById("korrumzthegame")
-const onlyfans = document.getElementById("onlyfans")
+function select(page) {
+    aboutmeButton.setAttribute("enabled", page === "aboutme" ? "true" : "false")
+    linksButton.setAttribute("enabled", page === "links" ? "true" : "false")
 
-aboutmeButton.addEventListener("click", () => {
-    aboutmeButton.setAttribute("enabled", "true")
-    linksButton.setAttribute("enabled", "false")
+    aboutme.style.display = page === "aboutme" ? "block" : "none"
+    linkList.style.display = page === "links" ? "block" : "none"
+}
 
-    aboutme.style.display = "block"
-    links.style.display = "none"
-})
-
-linksButton.addEventListener("click", () => {
-    links_button.setAttribute("enabled", "true")
-    aboutme_button.setAttribute("enabled", "false")
-
-    links.style.display = "block"
-    aboutme.style.display = "none"
-})
-
-youtube.addEventListener("click", () => {
-    open("https://www.youtube.com/channel/UCIINaeuHEOTmzEk0RvIprNQ", "_blank").focus()
-})
-
-github.addEventListener("click", () => {
-    open("https://github.com/CZUBIX", "_blank").focus()
-})
-
-discord.addEventListener("click", () => {
-    open("https://discord.com/users/636096693712060416", "_blank").focus()
-})
-
-discordPoligon.addEventListener("click", () => {
-    open("https://discord.gg/DFyFhtESq9", "_blank").focus()
-})
-
-cenzura.addEventListener("click", () => {
-    open("https://cenzura.poligon.lgbt", "_blank").focus()
-})
-
-steam.addEventListener("click", () => {
-    open("https://steamcommunity.com/id/czub1x", "_blank").focus()
-})
-
-roblox.addEventListener("click", () => {
-    open("https://www.roblox.com/users/2480082516/profile", "_blank").focus()
-})
-
-korrumzthegame.addEventListener("click", () => {
-    open("https://korrumzthegame.wtf", "_blank").focus()
-})
-
-onlyfans.addEventListener("click", () => {
-    open("https://onlyfans.com/czubix", "_blank").focus()
-})
-
-function poligonFunc() {
+function goTo(id) {
     linksButton.dispatchEvent(new Event("click"))
-    discordPoligon.style.backgroundColor = "#202225"
+    document.getElementById(id).style.backgroundColor = "#202225"
 
     setTimeout(() => {
-        discordPoligon.style.backgroundColor = "transparent"
+        document.getElementById(id).style.backgroundColor = "transparent"
     }, 300)
 }
 
-function cenzuraFunc() {
-    linksButton.dispatchEvent(new Event("click"))
-    cenzura.style.backgroundColor = "#202225"
-
-    setTimeout(() => {
-        cenzura.style.backgroundColor = "transparent"
-    }, 300)
+const links = {
+    youtube: "https://www.youtube.com/channel/UCIINaeuHEOTmzEk0RvIprNQ",
+    github: "https://github.com/CZUBIX",
+    discord: "https://discord.com/users/636096693712060416",
+    discord_poligon: "https://discord.gg/DFyFhtESq9",
+    cenzura: "https://cenzura.poligon.lgbt",
+    steam: "https://steamcommunity.com/id/czub1x",
+    roblox: "https://www.roblox.com/users/2480082516/profile",
+    korrumzthegame: "https://korrumzthegame.wtf",
+    onlyfans: "https://onlyfans.com/czubix"
 }
 
-function robloxFunc() {
-    linksButton.dispatchEvent(new Event("click"))
-    roblox.style.backgroundColor = "#202225"
-
-    setTimeout(() => {
-        roblox.style.backgroundColor = "transparent"
-    }, 300)
+for (let key in links) {
+    document.getElementById(key).addEventListener("click", () => {
+        open(links[key], "_blank").focus()
+    })
 }
